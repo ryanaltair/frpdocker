6f52846978b040af792a41b4234a90c6dd430d99
ryanaltair_quicklane <al_tair@live.com>
2019-04-09 21:58:21 +0800

    bugfixed

--- install_server.sh
+++ install_server.sh
@@ -1,7 +1,7 @@
 cp ./frps /usr/bin/
 mkdir /etc/frp
 cp ./frps.ini /etc/frp/
-mdkir /usr/lib/systemd/system
+mkdir /usr/lib/systemd/system
 cp ./systemd/frps.service /usr/lib/systemd/system/
 cd /usr/lib/systemd/system/ 
 systemctl enable frps
